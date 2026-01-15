53baeece38375365aaa140e1022d03eb66179c54
Simon Gloor <sgloor@outlook.com>
2026-01-15 13:02:38 +0100

    manage to apply collider

--- _assets/scripting/process_colliders.py
+++ _assets/scripting/process_colliders.py
@@ -1,7 +1,5 @@
 import omni.usd
-from pxr import Usd
-
-print("hello world")
+from pxr import Usd, UsdPhysics
 
 usd_context = omni.usd.get_context()
 stage = usd_context.get_stage()
@@ -11,5 +9,16 @@ prim_paths = selection.get_selected_prim_paths()
 
 for path in prim_paths:
     prim = stage.GetPrimAtPath(path)
-    if prim:
-        print(path, prim.GetTypeName())
+    if not prim or not prim.IsValid():
+        continue
+
+    # Apply a collider
+    UsdPhysics.CollisionAPI.Apply(prim)
+    physx_collision = UsdPhysics.MeshCollisionAPI.Apply(prim)
+    physx_collision.CreateApproximationAttr("convexHull")
+    print(f"Added convex hull collider to {path}")
+
+
+
+
+
